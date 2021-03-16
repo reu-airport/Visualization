7a6807e0c9921c120ad0d8e0d9a69566eeb5c687
Adlls <adlansaidullaev@gmail.com>
2021-03-16 23:43:12 +0300

    init mainScene on controller

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const mainScene = require('./../scenes/mainScene.ts');
+
+let ms = new mainScene();
+
 router.get('/', (req, res, next) => {
+    ms,
     res.send("ping");
 });
 
